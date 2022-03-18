98766702f93fbc2a83eb14593e2e00fa9ba9e608
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2022-03-18 20:46:22 +0100

    refactor: simplify handleChange

--- hooks/useHome.ts
+++ hooks/useHome.ts
@@ -1,4 +1,4 @@
-import useSWR, { useSWRConfig } from 'swr';
+import useSWR from 'swr';
 import { IProgress } from 'types/start-up.type';
 import { fetcher } from 'utils/fetcher';
 
@@ -14,8 +14,7 @@ interface IUseHome {
 
 export const useHome = (): IUseHome => {
 	const userId = 1;
-	const { mutate } = useSWRConfig();
-	const { data, error } = useSWR<IData>(
+	const { data, error, mutate } = useSWR<IData>(
 		`/api/start-ups/progress/${userId}`,
 		fetcher
 	);
@@ -25,55 +24,12 @@ export const useHome = (): IUseHome => {
 		progressId: number,
 		taskId: number
 	) => {
-		if (!data) {
-			return;
-		}
+		await fetch(`/api/start-ups/progress/${userId}`, {
+			method: 'PATCH',
+			body: JSON.stringify({ progressId, taskId, isChecked: checked }),
+		});
 
-		const currentProgresses = data.userProgresses;
-
-		const progressIndex = currentProgresses.findIndex(
-			(p) => p.id === progressId
-		);
-
-		if (progressIndex === -1) {
-			return;
-		}
-
-		const progress = currentProgresses[progressIndex];
-		if (!progress) {
-			return;
-		}
-
-		const taskIndex = progress.tasks.findIndex((task) => task.id === taskId);
-		if (taskIndex !== -1) {
-			// update progress task
-			progress.tasks[taskIndex].isChecked = checked;
-
-			// update progress by checking the number of tasks not completed is zero
-			const progressTasksNotChecked = progress.tasks.filter(
-				(task) => !task.isChecked
-			).length;
-
-			const isCompleted = progressTasksNotChecked === 0;
-
-			progress.isCompleted = isCompleted;
-			currentProgresses[progressIndex] = progress;
-
-			// update the local data immediately, but disable the revalidation
-			mutate(
-				`/api/start-ups/progress/${userId}`,
-				{ userProgresses: currentProgresses },
-				false
-			);
-
-			// Now, send data to API
-			await fetch(`/api/start-ups/progress/${userId}`, {
-				method: 'PATCH',
-				body: JSON.stringify({ progressId, taskId, isChecked: checked }),
-			});
-
-			mutate(`/api/start-ups/progress/${userId}`);
-		}
+		mutate();
 	};
 
 	return { data, error, handleChange };
