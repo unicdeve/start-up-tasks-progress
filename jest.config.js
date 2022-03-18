3107dab9db455a52952bab44eb6941af4d7618a5
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2022-03-18 22:56:20 +0100

    feat: unit tests config

---
+++ jest.config.js
@@ -0,0 +1,52 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+	dir: './',
+});
+
+const customJestConfig = {
+	collectCoverageFrom: [
+		'**/*.{js,jsx,ts,tsx}',
+		'!**/*.d.ts',
+		'!**/node_modules/**',
+	],
+	moduleNameMapper: {
+		/* Handle CSS imports (with CSS modules)
+    https://jestjs.io/docs/webpack#mocking-css-modules */
+		'^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+		// Handle CSS imports (without CSS modules)
+		'^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+		/* Handle image imports
+    https://jestjs.io/docs/webpack#handling-static-assets */
+		'^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
+			'<rootDir>/__mocks__/fileMock.js',
+		'^components/(.*)$': '<rootDir>/components/$1',
+		'^pages/(.*)$': '<rootDir>/pages/$1',
+		'^hooks/(.*)$': '<rootDir>/hooks/$1',
+		'^public/(.*)$': '<rootDir>/public/$1',
+		'^utils/(.*)$': '<rootDir>/utils/$1',
+		'^types/(.*)$': '<rootDir>/types/$1',
+		'^styles/(.*)$': '<rootDir>/styles/$1',
+	},
+	testPathIgnorePatterns: [
+		'<rootDir>/node_modules/',
+		'<rootDir>/.next/',
+		'<rootDir>/coverage/',
+	],
+	testEnvironment: 'jest-environment-jsdom',
+	transform: {
+		/* Use babel-jest to transpile tests with the next/babel preset
+    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+		'^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+	},
+	transformIgnorePatterns: [
+		'/node_modules/',
+		'^.+\\.module\\.(css|sass|scss)$',
+	],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+module.exports = createJestConfig(customJestConfig);
